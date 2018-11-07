4e20fb11c42f656c02ef886060b9d8c0ecae8360
Jaime Fernando Neis <jaimefneis@gmail.com>
2018-11-07 09:08:46 -0200

    First commit

---
+++ HelloGitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+            Console.ReadLine();
+        }
+    }
+}
